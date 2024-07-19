2b9947504091b7426a6d2c60ddce9f9ef36f572b
Cycro997 <152512005+Cycro997@users.noreply.github.com>
2024-07-19 21:35:55 +0300

    Deleted useless directory

--- CyTerminal/data/extracode/CTScript/test.py
+++
@@ -1,8 +0,0 @@
-#CTScript test script
-import CTScript as cts
-a=cts.ctscr("cts_terminal;3", database={"i":"3"}, options={"log":True})
-print(a) 
-print(repr(a))
-print(a.scrsplit())
-print(a.scrfullsplit())
-print(a.exec())
